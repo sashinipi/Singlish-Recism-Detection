a1078383b7a116ba7c8bdd97bf2e63925b24d53f
dulanj <dulanj@zone24x7.com>
2019-04-19 09:04:18 +0530

    Absolute path fix apk -> api

--- params.py
+++ params.py
@@ -4,54 +4,57 @@ Created on Mar 31, 2019
 @author: dulan
 '''
 from enum import Enum, unique
+import os
+import os.path as osp
+
+ABS_PATH_TO_FILE = os.path.dirname(__file__)
+
 @unique
 class PRED_TYPE(Enum):
-    SIMPLE_NN = 1
-    SVM = 2
-    MNB = 3
+    SIMPLE_NN   = 1
+    SVM         = 2
+    MNB         = 3
 
 class DICTIONARY:
-    SINGLISH_WORD = 'singlish'
-    WORD_COUNT = 'count'
+    SINGLISH_WORD   = 'singlish'
+    WORD_COUNT      = 'count'
 
 class DIR:
-    HOST_MACHINE_LOC = '/root/SRD/main/' # Use this for docker
-    # HOST_MACHINE_LOC = ''
-    DEF_SAV_LOC = HOST_MACHINE_LOC+'save'
-    LOGS_DIR = HOST_MACHINE_LOC+'logs'
+    DEF_SAV_LOC     = osp.join(ABS_PATH_TO_FILE, 'main/save')
+    LOGS_DIR        = osp.join(ABS_PATH_TO_FILE, 'main/logs')
 
 class FILES:
     TAG_DATA_FILENAME_EXCEL = ''
     TAG_DICT_FILENAME = ''
-    EXCEL_DATA_FILE_PATH = '/home/dulan/learn/sashini-fyp/code/git/Singlish-Recism-Detection/data/final-data-set.xlsx'
-    DICTIONARY_FILE_PATH = '/home/dulan/learn/sashini-fyp/code/git/Singlish-Recism-Detection/data/dict.json'
-    CSV_FILE_PATH = '/home/dulan/learn/sashini-fyp/code/git/Singlish-Recism-Detection/data/output.csv'
-    SEP_CSV_FILE_PATHS = '/home/dulan/learn/sashini-fyp/code/git/Singlish-Recism-Detection/data/output_{}.csv'
-    TEST_CSV_FILE_PATH = '/home/dulan/learn/sashini-fyp/code/git/Singlish-Recism-Detection/tests/test_output.csv'
+    EXCEL_DATA_FILE_PATH    = osp.join(ABS_PATH_TO_FILE, 'data/final-data-set.xlsx')
+    DICTIONARY_FILE_PATH    = osp.join(ABS_PATH_TO_FILE, 'data/dict.json')
+    CSV_FILE_PATH           = osp.join(ABS_PATH_TO_FILE, 'data/output.csv')
+    SEP_CSV_FILE_PATHS      = osp.join(ABS_PATH_TO_FILE, 'data/output_{}.csv')
+    TEST_CSV_FILE_PATH      = osp.join(ABS_PATH_TO_FILE, 'tests/test_output.csv')
 
 class MISC:
     TAG_SAVE_COUNT = 5
     CLASSES = ['Racist', 'Neutral']
 
 class LSTMP:
-    LSTM_MAX_WORD_COUNT = 60
-    FOLDS_COUNT = 5
-    VALIDATION_TEST_SIZE = 0.2
-    MAX_EPOCHS = 20
-    OUTPUT_DIR = 'save'
+    LSTM_MAX_WORD_COUNT     = 60
+    FOLDS_COUNT             = 5
+    VALIDATION_TEST_SIZE    = 0.2
+    MAX_EPOCHS              = 20
+    OUTPUT_DIR              = 'save'
     DATA_SET_CLASSES = {
         'Neutral': [0, 1],
         'Racist': [1, 0]
     }
 
 class SVMF():
-    BOW_FILENAME = 'svm_bow'
-    TFIDF_FILENAME = 'svm_tfidf'
-    MODEL_FILENAME = 'svm_model'
-    INPUT_FILENAME = 'svm_input'
+    BOW_FILENAME    = 'svm_bow'
+    TFIDF_FILENAME  = 'svm_tfidf'
+    MODEL_FILENAME  = 'svm_model'
+    INPUT_FILENAME  = 'svm_input'
 
 class MNB():
-    BOW_FILENAME = 'mnb_bow'
-    TFIDF_FILENAME = 'mnb_tfidf'
-    MODEL_FILENAME = 'mnb_model'
-    INPUT_FILENAME = 'mnb_input'
+    BOW_FILENAME    = 'mnb_bow'
+    TFIDF_FILENAME  = 'mnb_tfidf'
+    MODEL_FILENAME  = 'mnb_model'
+    INPUT_FILENAME  = 'mnb_input'
